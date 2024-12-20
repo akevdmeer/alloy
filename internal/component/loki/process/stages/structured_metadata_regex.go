24b07990b7c4beb1e83fb0810ad802d5ed19dfd8
Thomas Ake van der Meer <tvandermeer@epo.org>
2024-12-20 15:12:25 +0100

    Add new stage

---
+++ internal/component/loki/process/stages/structured_metadata_regex.go
@@ -0,0 +1,57 @@
+package stages
+
+import (
+	"regexp" 
+	"github.com/go-kit/log"
+	"github.com/grafana/alloy/internal/runtime/logging/level"
+	_ "github.com/prometheus/common/model"
+
+	"github.com/grafana/loki/v3/pkg/logproto"
+)
+
+type StructuredMetadataRegexConfig struct {
+        Regex string `alloy:"regex,attr"`
+}
+
+func newStructuredMetadataRegexStage(logger log.Logger, configs StructuredMetadataRegexConfig) (Stage, error) {
+
+	re, error := regexp.Compile(configs.Regex)
+
+	if error != nil {
+		return &structuredMetadataRegexStage{}, error
+	}
+
+	return &structuredMetadataRegexStage{
+		logger:       logger,
+		regex:        *re,
+	}, nil
+}
+
+type structuredMetadataRegexStage struct {
+	logger       log.Logger
+	regex        regexp.Regexp
+}
+
+func (s *structuredMetadataRegexStage) Name() string {
+	return StageTypeStructuredMetadataRegex
+}
+
+// Cleanup implements Stage.
+func (*structuredMetadataRegexStage) Cleanup() {
+	// no-op
+}
+
+func (s *structuredMetadataRegexStage) Run(in chan Entry) chan Entry {
+	return RunWith(in, func(e Entry) Entry {
+
+		for labelName, labelValue := range e.Labels {
+			if (s.regex.MatchString(string(labelName))) {
+				e.StructuredMetadata = append(e.StructuredMetadata, logproto.LabelAdapter{Name: string(labelName), Value: string(labelValue)})
+				delete(e.Labels, labelName)
+				//level.Debug(s.logger).Log("name", labelName, "value", labelValue)
+			}
+		}
+
+		return e
+	})
+}
